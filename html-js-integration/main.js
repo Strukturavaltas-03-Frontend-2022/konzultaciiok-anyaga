f2e444a543dcc06805d437098a52526343879edc
Joe Cserko <j.cserko@gmail.com>
2022-09-14 10:19:59 +0200

    Tests.

--- html-js-integration/main.js
+++ html-js-integration/main.js
@@ -1,4 +1,11 @@
+const samleUser = {
+    "id": 1,
+    "name": "Cad Bedford",
+    "email": "cbedford0@symantec.com", 
+    "category":"Guest"
+};
 const container = document.querySelector('.container');
+const span = document.querySelector('.pass');
 
 const button = document.createElement('button');
 container.appendChild(button);
@@ -15,7 +22,29 @@ button.addEventListener('click', function() {
     console.log(this);
 });
 
-input.addEventListener('keydown', (ev) => {
-    console.log(ev);
-    // ev.preventDefault();
+input.addEventListener('keyup', (ev) => {
+    // password: squirrel
+    if (input.value === 'squirrel') {
+        span.innerHTML = 'OK';
+    } else {
+        span.innerHTML = 'Bad Password!';
+    }
 });
+
+
+const addUser = async () => {
+    // const name = document.querySelector('.nameInput').value;
+    // const email = document.querySelector('.emailInput').value;
+    // const category = document.querySelector('.categoryInput').value;
+    await fetch('https://nettuts.hu/jms/anita/users', {
+        method: 'POST',
+        body: JSON.stringify({name: 'Kiss Anita', email: 'anita@gmail.com', category: 'user'}),
+        headers: {
+            'Content-type': 'application/json',
+        }
+    });
+
+
+};
+
+await addUser();
