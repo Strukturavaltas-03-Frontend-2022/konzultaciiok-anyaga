faaef5e82234debf1417356fc688353c7974a98d
Joe Cserko <j.cserko@gmail.com>
2022-09-14 10:57:18 +0200

    Update json example.

--- json-server-example/main.js
+++ json-server-example/main.js
@@ -2,14 +2,17 @@ import { apiUrl } from './assets/settings.js';
 
 // Create a new user.
 // {"id":1,"name":"Cad Bedford","email":"cbedford0@symantec.com","category":"Guest"}
-await fetch(`${apiUrl}users`, {
+const postResponse = await fetch(`${apiUrl}users`, {
     method: 'POST',
-    body: JSON.stringify({"name":"Szilágyi Viktor","email":"cbedford0@symantec.com","category":"Guest"}),
+    body: JSON.stringify({name:"Szilágyi Viktor",email:"cbedford0@symantec.com",category:"Guest"}),
     headers: {
         'Content-type': 'application/json',
     },
 });
+const newUser = await postResponse.json();
+console.log(newUser);
 
+// Get all of users.
 const response = await fetch(`${apiUrl}users`);
 const userList = await response.json();
 console.log(userList);
