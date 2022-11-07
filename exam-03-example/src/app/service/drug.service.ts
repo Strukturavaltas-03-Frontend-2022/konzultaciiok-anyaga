63e73ed2c255db6e4e14ebb9b1988cbb288a078d
Joe Cserko <j.cserko@gmail.com>
2022-11-07 13:19:53 +0100

    Store-based solution.

--- exam-03-example/src/app/service/drug.service.ts
+++ exam-03-example/src/app/service/drug.service.ts
@@ -46,3 +46,66 @@ export class DrugService {
   }
 
 }
+
+/**
+ * Store-based solution.
+ */
+
+/*
+export class DrugService {
+
+  apiUrl: string = environment.apiUrl;
+
+  entityName: string = 'drugs';
+
+  list$: BehaviorSubject<Drug[]> = new BehaviorSubject<Drug[]>([]);
+
+  constructor(
+    private http: HttpClient,
+  ) { }
+
+  getAll(): void {
+    this.http.get<Drug[]>(`${this.apiUrl}${this.entityName}`).subscribe(
+      list => this.list$.next(list),
+    );
+  }
+
+  get(id: number): Observable<Drug> {
+    return this.http.get<Drug>(`${this.apiUrl}${this.entityName}/${id}`);
+  }
+
+  create(drug: Drug): void {
+    this.http.post<Drug>(
+      `${this.apiUrl}${this.entityName}`,
+      drug
+    ).subscribe(
+      createdDrug => {
+        const list = this.list$.getValue();
+        list.push(createdDrug);
+        this.list$.next(list);
+      }
+    )
+  }
+
+  update(drug: Drug): Observable<Drug> {
+    return this.http.patch<Drug>(
+      `${this.apiUrl}${this.entityName}/${drug.id}`,
+      drug
+    );
+  }
+
+  delete(drug: Drug): void {
+    this.http.delete<Drug>(
+      `${this.apiUrl}${this.entityName}/${drug.id}`
+    ).subscribe(
+      response => {
+        const list = this.list$.getValue();
+        const index = list.findIndex( d => d.id === drug.id );
+        list.splice(index, 1);
+        this.list$.next(list);
+      }
+    );
+  }
+
+}
+*/
