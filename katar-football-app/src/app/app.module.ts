941b50d87eef8885e267a1ecc4b5922c85fc922b
Joe Cserko <j.cserko@gmail.com>
2022-11-22 09:21:07 +0100

    Firebase football app.

---
+++ katar-football-app/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavComponent } from './common/nav/nav.component';
+import { HomeComponent } from './page/home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
